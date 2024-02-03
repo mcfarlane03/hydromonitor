a1fa7b971a4eedfb3902322ab77ad7f4f1d33c08
Lui McFarlane <73975082+mcfarlane03@users.noreply.github.com>
2024-02-02 20:42:45 -0500

    defined call back function

--- backend/app/mqtt.py
+++ backend/app/mqtt.py
@@ -35,6 +35,8 @@ class MQTT:
 
         # 3. REGISTER CALLBACK FUNCTION(S) FOR EACH TOPIC USING THE self.client.message_callback_add("topic",self.function) FUNCTION
         # WHICH TAKES A TOPIC AND THE NAME OF THE CALLBACK FUNCTION YOU HAVE CREATED FOR THIS SPECIFIC TOPIC
+        self.client.message_callback_add("620156144", self.update) 
+        # self.client.message_callback_add("620156144_sub", self.toggle)
 
          
 
@@ -82,6 +84,18 @@ class MQTT:
    
 
     # 2. DEFINE CALLBACK FUNCTIONS(S) BELOW FOR EACH TOPIC(S) THE BACKEND SUBSCRIBES TO 
+    
+    def update(self, client, userdata, msg):
+        try:
+            topic   = msg.topic
+            payload = msg.payload.decode("utf-8")
+            # print(payload) # UNCOMMENT WHEN DEBUGGING  
+            
+            update  = loads(payload) # CONVERT FROM JSON STRING TO JSON OBJECT  
+            self.mongo.addUpdate(update) # INSERT INTO DATABASE 
+
+        except Exception as e:
+            print(f"MQTT: Update Error: {str(e)}")
      
 
 
